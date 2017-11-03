cef5a71d8f1ad1b1274992544aaebb72939fa686
JiHyung.Choi <choijh@ivisolution.com>
2017-11-03 18:26:30 +0900

    add JsonParserBrowser

---
+++ JsonDataBrowser/json_data_browser/data_manager/data_manager.hpp
@@ -0,0 +1,16 @@
+#ifndef DATA_MANAGER_H
+#define DATA_MANAGER_H
+
+#include <iostream>
+
+using namespace std;
+
+
+class DataManager
+{
+public:
+    DataManager();
+    virtual ~DataManager();
+};
+
+#endif // DATA_MANAGER_H
---
+++ JsonDataBrowser/json_data_browser/json_parser/json_parser.cpp
@@ -0,0 +1,11 @@
+#include "json_parser.hpp"
+
+JsonParser::JsonParser()
+{
+    cout << "JsonParser Start!!" << endl;
+}
+
+JsonParser::~JsonParser()
+{
+
+}
