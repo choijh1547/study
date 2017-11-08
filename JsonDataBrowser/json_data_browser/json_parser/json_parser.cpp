e29d8991da4faf1a6df98b87fb10c9cf05adcbb5
JiHyung.Choi <choijh@ivisolution.com>
2017-11-08 10:45:29 +0900

    [JsonDataBrowser] add exception

--- JsonDataBrowser/json_data_browser/json_parser/json_parser.cpp
+++ JsonDataBrowser/json_data_browser/json_parser/json_parser.cpp
@@ -26,10 +26,19 @@ void JsonParser::parsing()
 
     BOOST_FOREACH( boost::property_tree::ptree::value_type &vt, data )
     {
+
         boost::property_tree::ptree &children = data.get_child(vt.first);
+
+        m_dataManager->setData(vt.first, vt.second.data());
 
         BOOST_FOREACH( boost::property_tree::ptree::value_type &vt1, children )
         {
+            if( vt1.second.empty() )
+            {
+                cout << vt1.second.data() << endl;
+                m_dataManager->setData(vt1.first, vt1.second.data());
+            }
+
             BOOST_FOREACH( boost::property_tree::ptree::value_type &vt2, vt1.second )
             {
                 m_dataManager->setData(vt2.first, vt2.second.data());
